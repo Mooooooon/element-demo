80bca1a1618c72c778b8e9677d1d113882977ea7
moon <maoxianmoon@gmail.com>
2017-08-02 22:59:16 +0800

    first commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="wrapper">
+        <el-row :gutter="20">
+            <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-select v-model="value" placeholder="请选择">
+                        <el-option
+                                v-for="item in options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                        </el-option>
+                    </el-select>
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <chart :chartData="chartData"></chart>
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content bg-purple">
+                    <el-select v-model="value" placeholder="请选择">
+                        <el-option
+                                v-for="item in options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                        </el-option>
+                    </el-select>
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content bg-purple"></div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+  import Chart from './LandingPage/Chart.vue'
+
+  export default {
+    name: 'Landing',
+    data () {
+      return {
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: '',
+        chartData: [
+          0.9700, 0.9700, 0.9700, 0.9698, 0.9700, 0.9699, 0.9699, 0.9700, 0.9702, 0.9699
+        ]
+      }
+    },
+    components: {Chart}
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/renderer/components/LandingPage/Chart.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="chart">
+        <canvas id="myChart"></canvas>
+    </div>
+</template>
+
+<script>
+  import Chart from 'chart.js'
+
+  export default {
+    name: 'chart',
+    data () {
+      return {
+        myChart: null
+      }
+    },
+    props: ['chartData'],
+    mounted () {
+      let ctx = document.getElementById('myChart').getContext('2d')
+      this.myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: new Array(10),
+          datasets: [{
+            label: 'label',
+            data: this.chartData,
+            lineTension: 0,
+            fill: false,
+            pointRadius: 0,
+            borderWidth: 1,
+            borderColor: '#36a2eb'
+          }]
+        }
+      })
+    }
+  }
+</script>
